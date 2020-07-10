b34f6eddb0bb4d4c06e45a41a5828e8730b5e306
Pedro Higor de Sousa <phsousa@sacavalcante.com.br>
2020-07-10 02:07:31 -0300

    Add methods of application

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const requireDir = require('require-dir'); // Realiza a importação de todos os
 // Iniciando o APP
 const app = express();
 
+// Permite para que possa ser envido dados para aaplicação em formato de json
+app.use(express.json());
+
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true },);
 
